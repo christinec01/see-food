ec7f2f6a376a57158d485a2378030aca9e90611c
meredith-jones <meredithjw@gmail.com>
2016-11-25 17:44:39 -0800

    Add user class, migration, and controller.

---
+++ see-food/app/controllers/sessions_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+
+def login
+  @user = User.find_by_email(params[:email])
+  if @user.password == params[:password]
+    give_token
+  else
+    redirect_to home_url
+  end
+end
+
+end
--- see-food/db/schema.rb
+++ see-food/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161126002738) do
+ActiveRecord::Schema.define(version: 20161126013724) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20161126002738) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
+    t.string   "password_hash"
+    t.string   "email"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
