420e17aef4754a2ee133cf46c9718c43ad253184
Christine Casebolt <christine.casebolt@gmail.com>
2016-11-26 10:36:37 -0800

    adds react views to app

--- see-food/app/controllers/restaurants_controller.rb
+++ see-food/app/controllers/restaurants_controller.rb
@@ -2,11 +2,11 @@ class RestaurantsController < ApplicationController
 
   def index
     @restaurant = GooglePlaces::Client.new(ENV['GOOGLE_API_KEY'])
-    spots = @restaurant.spots(34.2746, -119.2290, types: 'restaurant')
+    @spots = @restaurant.spots(34.2746, -119.2290, types: 'restaurant')
 
-    respond_to do |f|
-      f.html { render json: spots }
-    end
+    # respond_to do |f|
+    #   f.html { render json: spots }
+    # end
   end
 
 end
---
+++ see-food/client/app/bundles/SeeFood/containers/SeeFoodContainer.jsx
@@ -0,0 +1,16 @@
+import React, { PropTypes } from 'react';
+import Main from '../components/Main';
+
+// Simple example of a React "smart" component
+export default class SeeFoodApp extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+  render() {
+    return (
+      <div>
+        <Main spots={this.props.spots} />
+      </div>
+    );
+  }
+}
