397b895554f61fb1ace05854f63bf3e0acc86a23
Timothy Tao <timctao@gmail.com>
2016-11-27 13:26:53 -0800

    Create likes join table

--- see-food/db/schema.rb
+++ see-food/db/schema.rb
@@ -10,15 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161127205934) do
+ActiveRecord::Schema.define(version: 20161127212037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "address"
+    t.string   "phone_number"
+    t.string   "website"
   end
 
   create_table "users", force: :cascade do |t|
