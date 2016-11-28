13f83b18560a60152010fd857bc991b4da675809
Timothy Tao <timctao@gmail.com>
2016-11-27 17:57:24 -0800

    Change to Yelp API; Modify controller to call yelp API

--- see-food/app/controllers/restaurants_controller.rb
+++ see-food/app/controllers/restaurants_controller.rb
@@ -1,23 +1,16 @@
 class RestaurantsController < ApplicationController
   def index
-    # @restaurant = GooglePlaces::Client.new(ENV['GOOGLE_API_KEY'])
-    # @spots = @restaurant.spots_by_query(params[:format], types: 'restaurant')
-    # .select{|spot| spot if spot.photos.length > 0 && spot.opening_hours['open_now'] == true}
-    #
-    # #logic here to restrict spot.image based on yelp.category
-    # @url = @spots.map do |spot|
-    #   spot.photos[0].fetch_url(800)
-    # end
-
     client = Yelp::Client.new({ consumer_key: ENV['CONSUMER_KEY'],
                             consumer_secret: ENV['CONSUMER_SECRET'],
                             token: ENV['TOKEN'],
                             token_secret: ENV['TOKEN_SECRET']
                           })
 
-    @restaurant = client.search('San Francisco', { term: 'food' })
+    @restaurants = client.search(params[:format], { term: 'food' })
 
-    p @restaurant
+    @spots = @restaurants.businesses
+
+    @url = @spots.map { |spot| spot.image_url }
 
   end
 
