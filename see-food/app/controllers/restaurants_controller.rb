f9e5e083adb1c2ca3345a7ca1907b3e4df939c39
Timothy Tao <timctao@gmail.com>
2016-11-27 18:40:43 -0800

    Enlarge images in restaurant controller

--- see-food/app/controllers/restaurants_controller.rb
+++ see-food/app/controllers/restaurants_controller.rb
@@ -1,16 +1,21 @@
 class RestaurantsController < ApplicationController
   def index
+
+    def enlarge_image(string)
+      string.gsub(/([m][s][.][j][p][g])/, "o.jpg")
+    end
+
     client = Yelp::Client.new({ consumer_key: ENV['CONSUMER_KEY'],
                             consumer_secret: ENV['CONSUMER_SECRET'],
                             token: ENV['TOKEN'],
                             token_secret: ENV['TOKEN_SECRET']
                           })
 
-    @restaurants = client.search(params[:format], { term: 'food' })
+    @restaurants = client.search(params[:format], { term: 'food', limit: 1 })
 
-    @spots = @restaurants.businesses
-
-    @url = @spots.map { |spot| spot.image_url }
+    @spots = @restaurants.businesses.select { |spot| spot.is_closed == false }
+  
+    @url = @spots.map { |spot| enlarge_image(spot.image_url) }
 
   end
 
