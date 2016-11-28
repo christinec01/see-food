4cd05f3f4b6e8097cd6ecb7a7a7abce8b2d762e9
Timothy Tao <timctao@gmail.com>
2016-11-27 16:25:06 -0800

    Add Yelp API

--- see-food/app/controllers/restaurants_controller.rb
+++ see-food/app/controllers/restaurants_controller.rb
@@ -1,19 +1,29 @@
 class RestaurantsController < ApplicationController
   def index
-    p "*"*20
-    p params[:format]
-    @restaurant = GooglePlaces::Client.new(ENV['GOOGLE_API_KEY'])
-    @spots = @restaurant.spots_by_query(params[:format], types: 'restaurant')
-    .select{|spot| spot if spot.photos.length > 0 && spot.opening_hours['open_now'] == true}
+    # @restaurant = GooglePlaces::Client.new(ENV['GOOGLE_API_KEY'])
+    # @spots = @restaurant.spots_by_query(params[:format], types: 'restaurant')
+    # .select{|spot| spot if spot.photos.length > 0 && spot.opening_hours['open_now'] == true}
+    #
+    # #logic here to restrict spot.image based on yelp.category
+    # @url = @spots.map do |spot|
+    #   spot.photos[0].fetch_url(800)
+    # end
 
-    @url = @spots.map do |spot|
-      spot.photos[0].fetch_url(800)
-    end
+    client = Yelp::Client.new({ consumer_key: ENV['CONSUMER_KEY'],
+                            consumer_secret: ENV['CONSUMER_SECRET'],
+                            token: ENV['TOKEN'],
+                            token_secret: ENV['TOKEN_SECRET']
+                          })
+
+    @restaurant = client.search('San Francisco', { term: 'food' })
+
+    p @restaurant
+
   end
 
-def show
+  def show
 
-end
+  end
 
 
 end
