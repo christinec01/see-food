ebce92cc4072343086ac1131c1e51acce5de3d34
Christine Casebolt <christine.casebolt@gmail.com>
2016-11-27 23:00:15 -0800

    adds a like and show likes button to pic index

--- see-food/client/app/bundles/SeeFood/startup/SeeFoodApp.jsx
+++ see-food/client/app/bundles/SeeFood/startup/SeeFoodApp.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactOnRails from 'react-on-rails';
 import SeeFoodContainer from '../containers/SeeFoodContainer';
 
-const SeeFoodApp = (props) => (
-  <SeeFoodContainer {...props} />
-);
+const SeeFoodApp = (props) => {
+  let spots = props.spots.map((spot, i) => {
+    spot.url = props.url[i];
+    return spot;
+  });
+
+  return (
+    <SeeFoodContainer {...props} spots={spots} />
+  );
+}
 
 // This is how react_on_rails can see the SeeFoodApp in the browser.
 ReactOnRails.register({ SeeFoodApp });
