67e1609a5a2dec4a993e88692f736144adcfe71f
Aiden <aiden@Darrens-MacBook-Air-2.local>
2016-11-28 14:55:56 -0800

    Adds routes for sessions

--- see-food/config/routes.rb
+++ see-food/config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   resources :landings
   resources :restaurants
   resources :users
-  
+    # Session Routes
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'landings#index'
 end
