df0b39abff1dd1d83aab633d43302bea2e5efefe
Aiden <aiden@Darrens-MacBook-Air-2.local>
2016-11-28 10:53:25 -0800

    Fixes issues with registration and adds zip code field

--- see-food/client/app/bundles/SeeFood/components/LandingPage.jsx
+++ see-food/client/app/bundles/SeeFood/components/LandingPage.jsx
@@ -45,7 +45,7 @@ class Form extends React.Component {
       <div className="container">
         <div className="modal-content zip-modal">
           <div className="modal-body">
-          {this.props.showUserLoginForm ? null : <h1>See Food</h1> }
+          {this.props.showUserLoginForm || this.props.showRegistrationForm ? null : <h1>See Food</h1> }
           {this.props.showRegistrationForm ? <h1>Register</h1> : null }
           {this.props.showUserLoginForm ? <h1>Log In</h1> : null }
           {(this.props.showUserLoginForm || this.props.showRegistrationForm) ? null : <form className="zip-form" action="/landings" method="POST">
@@ -55,11 +55,11 @@ class Form extends React.Component {
             
             <div className="modal-footer">
 
-              {this.props.showRegistrationForm ? null : <input onClick={this.props.handleRegistrationClick} type="submit" value="Register" className="landing-buttons btn btn-outline-secondary" /> }
+              {this.props.showRegistrationForm || this.props.showUserLoginForm ? null : <input onClick={this.props.handleRegistrationClick} type="submit" value="Register" className="landing-buttons btn btn-outline-secondary" /> }
               
               {this.props.showRegistrationForm ? <Registration /> : null}
 
-              {this.props.showUserLoginForm ? null : <input onClick={this.props.handleClick} type="submit" value="Login" className="landing-buttons btn btn-outline-secondary" /> }
+              {this.props.showUserLoginForm || this.props.showRegistrationForm ? null : <input onClick={this.props.handleClick} type="submit" value="Login" className="landing-buttons btn btn-outline-secondary" /> }
               
               {this.props.showUserLoginForm ? <UserLogin /> : null }
 
