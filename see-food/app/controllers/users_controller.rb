19bb3d8c5fb56fff34bdd940e51e43f0e06fe65a
tereg <teregalla@gmail.com>
2016-11-29 11:48:19 -0800

    Change route after registration so that user goes to restaurants index upon registration

--- see-food/app/controllers/users_controller.rb
+++ see-food/app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to '/'
+      redirect_to restaurants_path(@user.zip_code)
     end
   end
 
