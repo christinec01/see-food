48c8eb75de0777af1ee07f76409bd65ea87edc35
meredith-jones <meredithjw@gmail.com>
2016-11-28 16:28:10 -0800

    Add functionality on react end to send restaurant info to restaurant controller.

--- see-food/app/controllers/restaurants_controller.rb
+++ see-food/app/controllers/restaurants_controller.rb
@@ -20,20 +20,24 @@ class RestaurantsController < ApplicationController
 
   def create
     # expecting a spot from front end
-    @restaurant = Restaurant.new(
-      name: params[:name],
-      address: params[:address],
-      phone_number: params[:phone_number],
-      website: params[:website],
-      picture_url: params[:picture_url]
-      )
+    p "*"* 50
+    p params
+    p "*"* 50
+    @restaurant = Restaurant.new(restaurant_params)
     @restaurant.save
+    p @restaurant
 
     @like = Like.create(
-      user_id: current_user.id,
+      user_id: 1,
       restaurant_id: @restaurant.id
       )
 
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :picture_url)
   end
 
 
