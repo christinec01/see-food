b1acb0c845e16bcfa6f05813c98da97c9a3bf3ed
Timothy Tao <timctao@gmail.com>
2016-11-28 17:55:00 -0800

    Refactor index route in restaurants controller

--- see-food/app/controllers/restaurants_controller.rb
+++ see-food/app/controllers/restaurants_controller.rb
@@ -1,20 +1,9 @@
 class RestaurantsController < ApplicationController
+  include RestaurantsHelper
+
   def index
-
-    def enlarge_image(string)
-      string.gsub(/([m][s][.][j][p][g])/, "o.jpg")
-    end
-
-    client = Yelp::Client.new({ consumer_key: ENV['CONSUMER_KEY'],
-                            consumer_secret: ENV['CONSUMER_SECRET'],
-                            token: ENV['TOKEN'],
-                            token_secret: ENV['TOKEN_SECRET']
-                          })
-
-    @restaurants = client.search(params[:format], { term: 'food'})
-
+    @restaurants = search_by_zip(params[:format])
     @spots = @restaurants.businesses.select { |spot| spot.is_closed == false }
-
     @url = @spots.map { |spot| enlarge_image(spot.image_url) }
   end
 
--- see-food/app/helpers/restaurants_helper.rb
+++ see-food/app/helpers/restaurants_helper.rb
@@ -1,2 +1,16 @@
 module RestaurantsHelper
+  def enlarge_image(string)
+    string.gsub(/([m][s][.][j][p][g])/, "o.jpg")
+  end
+
+  def search_by_zip(zip_code)
+    client = Yelp::Client.new({
+      consumer_key: ENV['CONSUMER_KEY'],
+      consumer_secret: ENV['CONSUMER_SECRET'],
+      token: ENV['TOKEN'],
+      token_secret: ENV['TOKEN_SECRET']
+    })
+
+    client.search(zip_code, { term: "food" })
+  end
 end
