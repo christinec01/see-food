2a1ddc91cb50105a0b4afc52b7583dac4602476e
tereg <teregalla@gmail.com>
2016-11-30 22:26:14 -0800

    Create initial email template which allows user to send information about favorite restaurants to friends

--- see-food/app/mailers/example_mailer.rb
+++ see-food/app/mailers/example_mailer.rb
@@ -2,7 +2,12 @@ class ExampleMailer < ApplicationMailer
   default from: "seefoodreactor@gmail.com"
 
   def sample_email(params)
-    p params[:enterEmail]
+  
+    p "*&*" * 80
+    @liked_spots = params[:likedSpots]
+ 
+
+
     mail(to: params[:enterEmail], subject: 'sample email')
   end
 end
