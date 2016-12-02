530400f68f37cbce0a0dbc47ade00f0d660de250
Timothy Tao <timctao@gmail.com>
2016-12-01 16:45:19 -0800

    Change colors on nav bar

--- see-food/client/app/bundles/SeeFood/components/NavBar.jsx
+++ see-food/client/app/bundles/SeeFood/components/NavBar.jsx
@@ -10,21 +10,23 @@ export default class NavBar extends React.Component {
   render() {
 
     return (
-      <div className="row">
-        <nav className="navbar navbar-default">
-          <div className="col-md-10">
-            <div className="container-fluid">
-              <a className="navbar-brand nav-font-size" href="/landings">See Food</a>
-              <form className="zip-form nav-font-size" action="/landings" method="POST">
-                <input className="nav-search" type="text" name="zip" placeholder="Enter zip code" />
-                <input className="btn btn-default" type="submit" value="Submit" />
-              </form>
+      <div className="nav-color">
+        <div className="row">
+          <nav className="navbar navbar-default">
+            <div className="col-md-10">
+              <div className="container-fluid">
+                <a className="navbar-brand nav-font-size nav-link" href="/landings">See Food</a>
+                <form className="zip-form nav-font-size" action="/landings" method="POST">
+                  <input className="nav-search" type="text" name="zip" placeholder="Zip Code" />
+                  <input className="btn btn-default" type="submit" value="Go!" />
+                </form>
+              </div>
             </div>
-          </div>
 
-          {(this.props.user !== null) ? <UserLoggedInFeature user={this.props.user} /> : null}
+            {(this.props.user !== null) ? <UserLoggedInFeature user={this.props.user} /> : null}
 
-        </nav>
+          </nav>
+        </div>
       </div>
     );
   }
--- see-food/client/app/bundles/SeeFood/components/UserLoggedInFeature.jsx
+++ see-food/client/app/bundles/SeeFood/components/UserLoggedInFeature.jsx
@@ -4,8 +4,8 @@ class UserLoggedInFeature extends React.Component {
 	render() {
 		console.log("Im In UserLoggedInFeature")
 		return (
-			<div className="col-md-6 ta-right nav-font-size">
-        <span><a href={"/users/" + this.props.user.id + "/likes"}>My Likes</a> | <a href="/logout">Logout</a></span> 
+			<div className="col-md-2 ta-right nav-font-size">
+        <span><a href={"/users/" + this.props.user.id + "/likes"} className="nav-link">My Likes</a> | <a href="/logout" className="nav-link">Logout</a></span>
       </div>
 		)
 	}
